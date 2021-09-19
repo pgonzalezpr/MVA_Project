8239c912c2a0eae2ca406f9a5f608e6f6bee3d8f
pgonzalezpr <pgonzalezpr1@gmail.com>
2021-09-18 19:11:21 -0500

    Added ID Column

---
+++ DataPreProcessing.R
@@ -0,0 +1,28 @@
+# CLEARING ENVIRONMENT #
+
+rm(list=ls(all=TRUE))
+
+# IMPORTING REQUIRED LIBRARIES #
+
+library(rstudioapi)
+library(tibble)
+library(dplyr)
+
+# SETTING WORKSPACE #
+
+current_path <- getActiveDocumentContext()$path 
+setwd(dirname(current_path ))
+
+# LOADING DATASET AND ADDING ID COLUMN #
+
+rain_data <- read.csv("weatherAUSOriginal.csv")
+n <- nrow(rain_data)
+ID = c(1:n)
+new_rain_data <- add_column(rain_data, ID, .before=1)
+
+# SAVING NEW DATASET #
+
+write.table(new_rain_data, file = "weatherAUS.csv", sep = ",", na = "NA", dec = ".", row.names = FALSE, col.names = TRUE)
+
+
+
